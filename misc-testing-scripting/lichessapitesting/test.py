32369769658e8e682b91e2ba96c03533710de67f
Nathan <Nathanp9000@gmail.com>
2021-03-23 15:28:10 -0500

    H

--- misc-testing-scripting/lichessapitesting/test.py
+++ misc-testing-scripting/lichessapitesting/test.py
@@ -1,7 +1,9 @@
 import io
 import chess.pgn
 import lichess.api
+import lichess.pgn
 import re
+import requests
 import chess.svg
 import chess.engine
 from lichess.format import SINGLE_PGN
@@ -12,8 +14,9 @@ engine = chess.engine.SimpleEngine.popen_uci('stockfish_13_win_x64/stockfish_13_
 #takes in username and last 20 games
 user = input('Enter your lichess username: ')
 info = lichess.api.user(user)
-pgn = lichess.api.user_games(user, max=20, format=SINGLE_PGN)  # max param is for number of games, don't touch anything else on this line
+pgn = lichess.api.user_games(user, max=20, format=SINGLE_PGN, opening = 'true')  # max param is for number of games, don't touch anything else on this line
 print()
+
 #make sure user only enters a number between 1 and 20
 game_Num = input('Enter which game would you like? ')
 
@@ -31,8 +34,9 @@ with open(f'{user}.pgn', 'w') as f:  # creates a file with filename last20{user}
 
 pgn_ = io.StringIO(pgn_store[(int(game_Num) - 1) * 2]) 
 
-#reads in a game
+# #reads in a game
 game = chess.pgn.read_game(pgn_)
+
 board = game.board()
 #prints board and the moves in the game
 print(f'{board}\n')
@@ -46,7 +50,12 @@ check = 0
 for move in game.mainline_moves():  
     board.push(move)
     info = engine.analyse(board, chess.engine.Limit(depth=10))
+    
+    #prints score and board
     print(info['score'])
+    score_string =  int(str(info['score'])[12: (str(info['score']).find(',') - 1)])
+
+    print(score_string)
     print(f"{board}", end='\n\n\n')
     if board.is_check():
         check += 1
@@ -56,3 +65,39 @@ print(f"{check} checks")
 print(board)
 print(pgn_store[(int(game_Num) - 1) * 2 + 1])
 # username: khreate
+
+# find username of player and then find opponent -> Parameters include opponent
+
+
+# def search(username, input_month, input_year, input_opponent):
+#     games_raw = requests.get(f"https://api.chess.com/pub/player/{username}/games/{input_year}/{input_month}")
+#     if not input_opponent:
+#         for i in range(0, len(games_raw.json()['games'])):
+#             game = chess.pgn.read_game(io.StringIO(games_raw.json()['games'][i]['pgn']))
+#             date = (str(game.headers["Date"])).split('.')  # year, month, day
+#             year = date[0]
+#             month = date[1]
+#             day = date[2]
+#             enemy_username = ' '
+#             if game.headers["White"] == username:
+#                 enemy_username = game.headers["Black"]
+#             else:
+#                 enemy_username = game.headers["White"]
+#             winner = game.headers["Termination"].split(' ')[0]
+#             final_date = f'Game was played on {month}, {day}, {year} versus {enemy_username}; {winner} won.'
+#             print(final_date)
+#     else:
+#         for i in range(0, len(games_raw.json()['games'])):
+#             game = chess.pgn.read_game(io.StringIO(games_raw.json()['games'][i]['pgn']))
+#             if input_opponent in game.headers['Black'] or input_opponent in game.headers['White']:
+#                 date = (str(game.headers["Date"])).split('.')  # year, month, day
+#                 year = date[0]
+#                 month = date[1]
+#                 day = date[2]
+#                 if game.headers["White"] == username:
+#                     enemy_username = game.headers["Black"]
+#                 else:
+#                     enemy_username = game.headers["White"]
+#                 winner = game.headers["Termination"].split(' ')[0]
+#                 final_date = f'Game was played on {month}, {day}, {year} versus {enemy_username}; {winner} won.'
+#                 print(final_date)
