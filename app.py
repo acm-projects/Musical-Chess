becd75b163a0ca49c70dc2bce6dbafb686bf7e3d
Suraj <surajkhosla79@gmail.com>
2021-04-22 19:20:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -52,21 +52,24 @@ def get_games_no_opponent(name, year, month):
             day = date[2]
             end_position = game.headers['CurrentPosition']
             result = game.headers['Termination'].split(' ')[3]
+
             if result == 'game':
                 result = 'abandoned'
             if game.headers["White"].lower() == name.lower():
                 enemy_username = game.headers["Black"]
                 colors['name'] = 'White'
+                elo = game.headers["WhiteElo"]
                 colors['opponent'] = 'Black'
             else:
                 enemy_username = game.headers["White"]
                 colors['name'] = 'Black'
+                elo = game.headers["BlackElo"]
                 colors['opponent'] = 'White'
             winner = game.headers["Termination"].split(' ')[0]
             api_result[i] = {'name': name, 'year': yearx, 'month': monthx, 'day': day, 'opponent': enemy_username,
                              'result': result, 'winner': winner, 'end': end_position, 'moves': moves_list,
                              'scores': scores,
-                             'colors': colors}
+                             'colors': colors, 'elo': elo}
         else:
             pass
     return jsonify(api_result)
@@ -99,15 +102,17 @@ def get_games(name, year, month, opponent):
             if game.headers["White"].lower() == name.lower():
                 enemy_username = game.headers["Black"]
                 colors['name'] = 'White'
+                elo = game.headers["WhiteElo"]
                 colors['opponent'] = 'Black'
             else:
                 enemy_username = game.headers["White"]
                 colors['name'] = 'Black'
+                elo = game.headers["BlackElo"]
                 colors['opponent'] = 'White'
             winner = game.headers["Termination"].split(' ')[0]
             api_result[i] = {'name': name, 'year': yearx, 'month': monthx, 'day': day, 'opponent': enemy_username,
                              'result': result,
-                             'winner': winner, 'end': end_position, 'scores': scores}
+                             'winner': winner, 'end': end_position, 'scores': scores, 'elo': elo}
             return jsonify(api_result)
 
 
